ee2ddbedabbe76fce3fd43e884ad23dbcadc57b9
cameron burns <cameronrobertburns@gmail.com>
2016-09-28 11:57:33 +1000

    added customController

---
+++ Overrides/CustomController.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by Cameron Burns.
+ * Date: 28/09/2016
+ */
+
+namespace VisageFour\Bundle\ToolsBundle\Overrides;
+
+use Doctrine\ORM\EntityManager;
+use Monolog\Logger;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CustomController extends Controller
+{
+    /*
+    IMPLEMENTATION CODE:
+    
+    $accessRole = 'SEND_PHOTOS';
+    $this->checkAccess ($accessRole);
+    $thisPerson = $this->getThisPerson();
+
+    */
+    public function checkAccess ($role) {
+        die ('needs to be configured for more generalized use - code below from anchorcards app');
+        /** @var $userSecurity \VisageFour\Bundle\PersonBundle\Services\UserSecurity */
+        $userSecurity   = $this->container->get('usersecurity');
+        $userSecurity->checkRole ($role);
+
+        return true;
+    }
+
+    public function getThisPerson () {
+        die ('needs to be configured for more generalized use - code below from anchorcards app');
+        /** @var $userSecurity \VisageFour\Bundle\PersonBundle\Services\UserSecurity */
+        $userSecurity   = $this->container->get('usersecurity');
+        $thisPerson     = $userSecurity->getPersonLoggedIn();
+
+        if (empty($thisPerson)) {
+            // create new person if email address exists
+            $thisUser = $userSecurity->getUserLoggedIn();
+
+            if (!empty($thisUser->getEmail())) {
+                /** @var PersonManager $personManager */
+                /** @var EntityManager $em */
+                /** @var Logger $logger */
+                $personManager  = $this->container->get('platypuspie.personmanager');
+                $em             = $this->container->get('doctrine.orm.default_entity_manager');
+                $logger         = $this->container->get('logger');
+
+                $person = $personManager->getOneBy(array ('email' => $thisUser->getEmail()));
+
+                //todo: move this all into the personManager
+                // check if person with email address already exists
+                if (!empty($person)) {
+                    throw new \Exception ('trying to create person with email address: "'. $thisUser->getEmail() .'" however a person with this email address already exists');
+                }
+
+                $logger->info('creating new $person obj as one did not exist for $user object.');
+
+                $person = $personManager->createNew();
+                $person->setEmail($thisUser);
+
+                $em->persist($person);
+                $em->flush();
+            } else {
+                throw new \Exception ('User does not have a related person object and could not find email addres from user object to create a new person.');
+            }
+        }
+
+        return $thisPerson;
+    }
+
+    // return true if is in dev environment
+    public function isDevEnvironment () {
+        if ($this->container->get('kernel')->getEnvironment() == 'dev') {
+            return true;
+        }
+
+        return false;
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ToolsBundle
 General Symfony tools used in VisageFour projects
 
-The tools Bundle includes the following tools:
+The tools Bundle includes the following:
 
 1. Code
   * Used for generating codes. Can be extended for particular use cases.
@@ -13,8 +13,14 @@ The tools Bundle includes the following tools:
 2. WebHookManager
   * pulls relevant details from entity and passes to the selected URL - currently used for connecting to Zapier
 
-3. Slug
+3. Slug entity
   * Used for resolving URLs to relevant objects
   * dependencies: Code Entity
+
+4. Code entity
+
+
+5. Custom controller extension
+  * Has isDevEnvironment(), checkAccess(), getThisPerson() etc
 
 
