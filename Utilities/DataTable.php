af06f897ff8116497e05ad1d3a65a36d8cdeffdf
cameron burns <cameronrobertburns@gmail.com>
2016-12-15 15:23:33 +1100

    updates to DataTable

--- Utilities/DataTable.php
+++ Utilities/DataTable.php
@@ -10,8 +10,8 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Psr\Log\LoggerInterface;
 use Swift_Mailer;
 
-// this class is used to represent dataa that will be
-// rendered in a datatable
+// this class is used to represent data that will be
+// rendered in a datatable or into a .CSV
 class DataTable
 {
     /*
@@ -26,23 +26,82 @@ class DataTable
     $datatable = new DataTable ($tableHeader, $tableData);
     // */
 
-    protected $header;
+    protected $headers;
     protected $data;
+    protected $CssClassName;
 
-    public function __construct($tableHeader, $tableData) {
+    public function __construct($tableHeaders, $tableData, $CssClassName = 'datatable1') {
         $this->setData($tableData);
-        $this->setHeader($tableHeader);
+        $this->setHeaders($tableHeaders);
+        $this->CssClassName = $CssClassName;
     }
 
-    public function setHeader ($tableHeaders) {
-        $this->tableHeaders = $tableHeaders;
+    public function setHeaders ($tableHeaders) {
+        $this->headers = $tableHeaders;
 
         return $this;
     }
 
     public function setData ($tableData) {
-        $this->tableData = $tableData;
+        $this->data = $tableData;
 
         return $this;
+    }
+
+    public function render ($renderDefaultStyle = true) {
+        if ($renderDefaultStyle) { $this->renderDefaultStyle(); }
+
+
+        if (empty($this->headers)) {
+            throw new \Exception ('dataTable headers must not be empty');
+        }
+        if (empty($this->data)) {
+            throw new \Exception ('dataTable data must not be empty');
+        }
+
+        $class = (!empty($this->CssClassName)) ? ' class="'. $this->CssClassName .'"' : '';
+        print '<table'. $class .'>';
+        // render table headers
+        print '    <tr>';
+        foreach ($this->headers as $curi => $curHeader) {
+            print '        <th>'. $curHeader['caption'] .'</th>';
+        }
+        print '        </tr>';
+
+        // render table data
+        foreach ($this->data as $curi1 => $curCell) {
+            print '        <tr>';
+            foreach ($this->headers as $curi2 => $curHeader) {
+                //if (!isset($curCell[$curHeader['reference']])) { die ('here'. $curHeader['reference']); }
+                $curValue = (isset($curCell[$curHeader['reference']])) ? $curCell[$curHeader['reference']] : '';
+                print '        <td>' . $curValue . '</td>';
+            }
+            print '        </tr>';
+        }
+        print '</table>';
+
+        return null;        // if return true, will render as : '1'
+    }
+
+    // provide some basic default styling for the datatable - this can be customized later
+    public function renderDefaultStyle () {
+        print '
+<style>
+.datatable1, .datatable1 th, .datatable1 td {
+    border: 4px solid white;
+    border-collapse: collapse;
+
+}
+.datatable1 th {
+    padding: 12px;
+    font-weight: bold;
+    background-color: #ddd;
+}
+.datatable1 td {
+    padding: 7px;
+    background-color: #eee;
+}
+</style>
+';
     }
 }
