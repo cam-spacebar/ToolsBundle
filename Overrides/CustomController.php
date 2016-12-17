3b67d77c422d5c66573621d524feacdf67eb8671
cameron burns <cameronrobertburns@gmail.com>
2016-12-17 15:37:04 +1100

    fixed error in CustomController getPersonLoggedInOrCreate() method > error linking new person to user when created.

--- Overrides/CustomController.php
+++ Overrides/CustomController.php
@@ -8,6 +8,7 @@ namespace VisageFour\Bundle\ToolsBundle\Overrides;
 
 use Doctrine\ORM\EntityManager;
 use Monolog\Logger;
+use Platypuspie\AnchorcardsBundle\Entity\Person;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CustomController extends Controller
@@ -18,55 +19,46 @@ class CustomController extends Controller
     $accessRole = 'SEND_PHOTOS';
     $this->checkAccess ($accessRole);
     $thisPerson = $this->getThisPerson();
-
     */
-    //this will check the user has the role specified in the parameters
-    public function checkAccess ($role) {
-        /** @var $userSecurity \VisageFour\Bundle\PersonBundle\Services\UserSecurity */
-        $userSecurity   = $this->container->get('usersecurity');
-        $userSecurity->checkRole ($role);
 
-        return true;
-    }
-
-    // will return the person that corresponds to the user account - you will need to make sure the relationships correctly.
-    // if the person doesn't exist, this method should create one for the user and return it.
-    public function getThisPerson () {
-        // This needs to be generalized for use in other apps!!! see service: platypuspie.personmanager which is specifc to anchorcards
-
-        /** @var $userSecurity \VisageFour\Bundle\PersonBundle\Services\UserSecurity */
-        $userSecurity   = $this->container->get('usersecurity');
-        $thisPerson     = $userSecurity->getPersonLoggedIn();
+    // creates a new Person object that matches to a user obj
+    // need to pass in the applications extended PersonManager for FindOneBy method - although BasePersonRepo will act as an interface
+    public function getPersonLoggedInOrCreate () {
+        $personManager  = $this->getPersonManager();
+        $thisPerson     = $this->getLoggedInPerson();
 
         if (empty($thisPerson)) {
             // create new person if email address exists
-            $thisUser = $userSecurity->getUserLoggedIn();
+            $thisUser = $this->getUserLoggedIn();
+
+            $logger = $this->getLogger();
+            $logger->info('User with username: "'. $thisUser->getUsername() .'" does not have a related Person object. Attempting to create');
 
             if (!empty($thisUser->getEmail())) {
-                /** @var PersonManager $personManager */
-                /** @var EntityManager $em */
-                /** @var Logger $logger */
-                $personManager  = $this->container->get('platypuspie.personmanager');
-                $em             = $this->container->get('doctrine.orm.default_entity_manager');
-                $logger         = $this->container->get('logger');
-
-                $person = $personManager->getOneBy(array ('email' => $thisUser->getEmail()));
-
-                //todo: move this all into the personManager
                 // check if person with email address already exists
+                $person = $personManager->findByEmail ($thisUser->getEmail());
+
                 if (!empty($person)) {
                     throw new \Exception ('trying to create person with email address: "'. $thisUser->getEmail() .'" however a person with this email address already exists');
                 }
 
                 $logger->info('creating new $person obj as one did not exist for $user object.');
 
+
+                /** @var Person $person */
                 $person = $personManager->createNew();
-                $person->setEmail($thisUser);
+                $person->setEmail($thisUser->getEmail());
+
+                $person->setRelatedUser($thisUser);
+                $thisUser->setRelatedPerson($person);
+
+                $em = $this->getEm();
 
                 $em->persist($person);
+                $em->persist($thisUser);
                 $em->flush();
             } else {
-                throw new \Exception ('User does not have a related person object and could not find email addres from user object to create a new person.');
+                throw new \Exception ('User does not have a related person object and could not find email address from user object to create a new person.');
             }
         }
 
@@ -92,5 +84,59 @@ class CustomController extends Controller
 
     public function getNoReplyEmail () {
         $this->container->getParameter('email_noreply');
+    }
+
+
+
+    public function checkAccess ($role) {
+        $userSecurity   = $this->getUserSecurity();
+        $userSecurity->checkRole ($role);
+
+        return true;
+    }
+
+    public function getLoggedInPerson () {
+        $userSecurity   = $this->getUserSecurity();
+        $thisPerson     = $userSecurity->getPersonLoggedIn();
+        return $thisPerson;
+    }
+
+    public function getUserLoggedIn () {
+        $userSecurity   = $this->getUserSecurity();
+        $thisUser       = $userSecurity->getUserLoggedIn();
+        return $thisUser;
+    }
+
+    /**
+     * @return Logger
+     */
+    public function getLogger () {
+        return $this->container->get('logger');
+    }
+
+
+
+    // METHODS TO OVERRIDE IN APP'S IMPLEMENTATION
+    // todo: should be made abstract?
+    /**
+     * @return \VisageFour\Bundle\PersonBundle\Services\UserSecurity
+     */
+    public function getUserSecurity () {
+        return $this->container->get('usersecurity');
+    }
+
+    /**
+     * @return \Platypuspie\AnchorcardsBundle\Services\PersonManager
+     */
+    public function getPersonManager () {
+        return $this->container->get('platypuspie.personmanager');
+    }
+
+    /**
+     * @return EntityManager
+     */
+    public function getEM () {
+        /** @var EntityManager $em */
+        return $this->container->get('doctrine.orm.default_entity_manager');
     }
 }
