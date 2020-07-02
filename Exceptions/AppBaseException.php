32582a9313e90d081e64201d321cf78a0b259c88
camburns <cameronrobertburns@gmail.com>
2020-07-02 19:04:04 +1000

    added AppBaseException.php

---
+++ Exceptions/AppBaseException.php
@@ -0,0 +1,26 @@
+<?php
+/*
+* created on: 02/07/2020 at 6:27 PM
+* by: cameronrobertburns
+*/
+
+namespace App\VisageFour\Bundle\ToolsBundle\Exceptions;
+
+abstract class AppBaseException extends \Exception
+{
+    /**
+     * This is the message that will be visible to the user (ussually via a flashbag)
+     * as $this->message will contain additional information (object ids etc) that we don't want
+     * shown to the user, but that we do want in a production error log.
+     *
+     * The publicMsg should be set in the inheriting exceptions __constructor
+     *
+     * @var string
+     */
+    protected $publicMsg;
+
+    public function getPublicMessage()
+    {
+        return $this->publicMsg;
+    }
+}
